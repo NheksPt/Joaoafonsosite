0efd7c489f95ea5ddaa19ebbdafaf5c8dbc07f49
NheksPt <moriarty82@hotmail.com>
2023-11-25 23:15:18 +0000

    Add project files.

---
+++ Joaoafonsosite.Domain/Entities/Trabalhos.cs
@@ -0,0 +1,16 @@
+﻿namespace Joaoafonsosite.Domain.Entities
+{
+    public class Trabalhos
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Tipo { get; set; }
+        public string? Descricao { get; set; }
+        public double Tamanho { get; set; }
+        public string? SoftwareUsado { get; set; }
+        public double Duracao { get; set;}
+        public string? Imagem { get; set; }
+        public DateTime? DataDeCriacao { get; set; }
+        public DateTime? DataDeEdicao { get; set; }
+    }
+}
---
+++ Joaoafonsositeweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Joaoafonsositeweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
